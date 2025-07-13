a87f2c1f04560a5ce56347aee31bdea600af4330
tparhipov <timur1arkhipov@gmail.com>
2025-07-13 15:37:11 +0400

    3

--- dist/services/outbox-init.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { OutboxMigrationService } from './outbox-migration.service';
-export declare class OutboxInitService implements OnModuleInit {
-    private readonly outboxMigration;
-    private readonly logger;
-    constructor(outboxMigration: OutboxMigrationService);
-    onModuleInit(): Promise<void>;
-}
-//# sourceMappingURL=outbox-init.service.d.ts.map
